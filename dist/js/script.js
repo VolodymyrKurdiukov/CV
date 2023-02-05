115e46ade707c7f8bbbc18d4e2c5328f1d600488
VolodymyrKurdiukov <volodymyr.kurdiukov@gmail.com>
2023-02-05 13:13:17 +0100

    final

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,3 +7,32 @@ function ibg() {
 	}
 }
 ibg();
+$(document).ready(function () {
+	$(window).scroll(function () {
+		let top = $(document).scrollTop();
+		if (top > 50) {
+
+			$(".header").css({
+				boxShadow: 'rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px',
+				transition: ' all 0.3s ease',
+				background: '#fff'
+			});
+
+
+		} else {
+			$(".header").css({
+				boxShadow: 'none',
+				transition: ' all 0.3s ease',
+				background: 'none'
+			});
+		}
+	});
+
+	$("a.header__link").on("click", function (e) {
+		e.preventDefault();
+		var anchor = $(this).attr('href');
+		$('html, body').stop().animate({
+			scrollTop: $(anchor).offset().top - document.querySelector('header').offsetHeight
+		}, 800);
+	});
+});
